081626613c1465b292d73b652fb4b23733822813
Freeeeeeeeenzy <75056113+FrenzY8@users.noreply.github.com>
2021-12-21 12:04:20 +0700

    21/12/2021

---
+++ worlds.js
@@ -0,0 +1,77 @@
+// Earth / Worlds made in JavaScript(Nodejs)
+
+/*** THE MOST IMPORTANT ***/
+// Worlds important
+const sick = false;
+const virusinvected = false;
+const rich = false;
+const poor = true;
+// Worlds material.
+const oxygen = require("oxygen");
+const tree = require("tree");
+const sky = require("sky"); // Beta sky
+const land = require("worlds-land");
+const Sun = require("worlds-sun"); // We have no moons. so there is no Night.
+
+function they(name) {
+	const Genders = new Gender();
+	if(Genders == "Male" || Genders == "Boy") {
+		// They boy.
+		set.name("Jack");
+	} else {
+		// They girl OwO
+  set.name("Elvie");
+	}
+}
+
+
+function life(100) { // (100) is a your life counter.
+	if(sick == false) {
+		// We do a normal day
+load(oxygen);
+load(tree);
+load(sky);
+load(land);
+load(sun);
+console.log("a normal day.");
+	} else {
+		if(sick == true) { // they sick.
+			const GetDisease = new Disease();
+			if (GetDisease == "COVID" || GetDisease == "COVID-19") {
+				var oxygen = new oxgyen();
+				// We reduce the oxygen
+// But wait let we check. they rich?
+if(they == rich) {
+	// Reduce the money
+let money = new WorldMoney();
+money.reduce('100$');
+} else {
+	// They poor.
+	// we only reduce the oxygen.
+	oxygen.reduce(15); // 15% Oxygen reduced.
+}
+			}
+		}
+	}
+}
+
+function normal(day) {
+	const theyStatus = new GetStatus();
+	if(theyStatus == 'Lazy' || theyStatus == 'lazy') {
+		
+		// New.
+		let money = new WorldMoney();
+		let theyMoney = new getTheyMoney();
+		// Check their moods
+if(theyMoods == "wasting-money") {
+	money.reduce(cost);
+} else {
+	money.keep(theMoney);
+}
+		// money.keep(theyMoney);
+	}
+}
+
+// Next Update Soon
+
+/*** Created in 21/12/2021 ***/
